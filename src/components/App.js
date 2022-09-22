44f24dc717e16d091712e05135be89e6567784e3
Nana <nanaolomaiyani@gmail.com>
2022-09-22 15:15:57 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,12 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <main>
+      <aside>
+     <header>You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck!</header> 
+      </aside>
+      </main>
     </div>
   );
 }
